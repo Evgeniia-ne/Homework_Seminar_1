cf0a5b169fa41b2256e8a286fbd7dd9cd7a27f9a
Asus <koncarik@mail.ru>
2022-11-28 00:27:54 +0500

    Задача решена, выводится ряд четных чисел до заданного числа

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
+int count = 2;
+int number = int.Parse(Console.ReadLine());
+while (count<number)
+{
+    Console.Write ($"{count} ,");
+    count = count+2;
+}
+Console.Write ($"{number}");
