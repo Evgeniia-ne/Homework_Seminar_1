948c3f3306bd467827da06d3598dddd5df03f09d
Asus <koncarik@mail.ru>
2022-11-27 23:45:01 +0500

    Задача решена, максимальное число из трех находится

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+int max;
+max = number1;
+if (number2>max) 
+   {
+        max = number2;
+    }
+if (number3>max) 
+{
+        max = number3;
+        }
+
+    Console.WriteLine ($"Максимальное число {max}");
