af755bd6d9399c2aa5c3cda82885d3db491da117
Asus <koncarik@mail.ru>
2022-11-27 22:20:23 +0500

    Задача 2 решена

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+{
+if (number1>number2)
+   {
+        Console.WriteLine ($"Максимальное число {number1}, минимальное число {number2}");
+    }
+else
+    {
+    Console.WriteLine ($"Максимальное число {number2}, минимальное число {number1}");
+    }
+}
