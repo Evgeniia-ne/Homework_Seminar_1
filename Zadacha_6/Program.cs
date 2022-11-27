4c045e78dbba415b0892b5ce47c651f7d1e69d49
Asus <koncarik@mail.ru>
2022-11-28 00:05:42 +0500

    Задача решена, программа выводит четное или нечетное введено число

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine ("Четное число");
+}
+else
+{
+    Console.WriteLine ("Нечетное число");
+}
